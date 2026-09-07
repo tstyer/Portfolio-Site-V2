972fd893354ce85100b06b6da35348607bdf6e19
tstyer <travisstyer.ts@gmail.com>
2026-09-07 11:02:26 +0100

    Create first project router

--- backend/routes/projectRoutes.ts
+++ backend/routes/projectRoutes.ts
@@ -1,2 +1,10 @@
-import Router from 'express';
+import { Router } from 'express';
+import { getAllProjects } from '../controllers/projectController.js';
+
+const router = Router();
+
+router.get('/', getAllProjects); // when a get request comes from '/', pass it the getAllProjects controller function.
+
+export default router; // default because this will be the only code in this file to export.
+
 
