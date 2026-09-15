c2a9e32e7625caee1bffac9cbfaeb56f9401d77a
tstyer <travisstyer.ts@gmail.com>
2026-09-15 11:28:09 +0100

    Add basic spacing to nav bar

--- frontend/src/pages/home_page/HomePage.tsx
+++ frontend/src/pages/home_page/HomePage.tsx
@@ -1,7 +1,9 @@
+import { NavBar } from "../../components/Navbar"
 
 export function HomePage() {
     return(
         <div>
+            <NavBar />
             <h1>Home</h1>
         </div>
     )
