0ed1a4af558b9ca2a526b58f7e93cb34b3ab5882
tstyer <travisstyer.ts@gmail.com>
2026-09-16 11:30:20 +0100

    Add basic content for header

--- frontend/src/components/nav_bar/Navbar.tsx
+++ frontend/src/components/nav_bar/Navbar.tsx
@@ -4,15 +4,17 @@ import { Link } from 'react-router-dom'
 
 export function NavBar() {
     return(
-        <div className='flex w-full h-auto justify-between px-3 py2'>
+        <div className='flex w-full h-auto justify-between'>
+            
             <div> 
-                <Link to={"/"} className='font-bold font-stretch-expanded'>Travis.</Link>
+                <Link to={"/"} className='font-bold font-stretch-expanded font-heading'>Travis.</Link>
             </div>
            
             <div>
-                <nav className='flex gap-6 font-heading'>
-                    <Link to={"/blog"} className='font-stretch-expanded'>BLOGS</Link>
-                    <Link to={"/contact"} className='font-stretch-expanded'>CONTACT</Link>
+                <nav className='flex gap-12 font-heading'>
+                    <Link to={"/"} className='font-stretch-expanded text-sm'>HOME</Link>
+                    <Link to={"/blog"} className='font-stretch-expanded text-sm'>BLOG</Link>
+                    <Link to={"/contact"} className='font-stretch-expanded text-sm'>CONTACT</Link>
                 </nav>
             </div>
             
--- frontend/src/pages/home_page/HomePage.tsx
+++ frontend/src/pages/home_page/HomePage.tsx
@@ -1,10 +1,31 @@
 import { NavBar } from "../../components/nav_bar/Navbar"
+import { Link } from "react-router"
 
 export function HomePage() {
     return(
-        <div>
+        <div className="px-8 py6">
+
             <NavBar />
-            <h1 className="font-heading">Home</h1>
+            
+            {/* Hero */}
+            <div className="flex-col pt-8">
+                <div className="px-3 py2">
+                    <h1 className="font-heading font-semibold text-2xl">Full-Stack Developer. <br />SEO Engineer. <br />Copywriter.</h1>
+                    <h2 className="font-text text-sm pt-2">Website Design, Full-Stack Development, SEO implementation and ethical copywriting,<br /> 
+                    I'll present your business that leads to more customers.
+                    </h2>
+                </div>
+
+                <div className="flex font-text text-xs mt-3 mx-2 gap-3 sm:gap-3 md:gap-6">
+                    <Link to={"/contact"} className="px-2 py-1 border border-solid border-emerald-100 bg-[#D6D3CC] transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
+                        Let's Chat
+                    </Link>
+                    <button className="px-2 py-1 border border-solid border-emerald-100 transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
+                        View Work
+                    </button>
+                </div>
+            </div>
+            
         </div>
     )
 }
