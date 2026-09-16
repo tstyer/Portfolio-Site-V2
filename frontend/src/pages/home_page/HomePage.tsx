7f41b6ec8be2d11654e9d6548e9f79c06fbdd30d
tstyer <travisstyer.ts@gmail.com>
2026-09-16 11:32:07 +0100

    Add styles to buttons

--- frontend/src/pages/home_page/HomePage.tsx
+++ frontend/src/pages/home_page/HomePage.tsx
@@ -17,10 +17,10 @@ export function HomePage() {
                 </div>
 
                 <div className="flex font-text text-xs mt-3 mx-2 gap-3 sm:gap-3 md:gap-6">
-                    <Link to={"/contact"} className="px-2 py-1 border border-solid border-emerald-100 bg-[#D6D3CC] transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
+                    <Link to={"/contact"} className="px-6 py-2 font-medium border border-solid border-emerald-100 bg-[#D6D3CC] transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
                         Let's Chat
                     </Link>
-                    <button className="px-2 py-1 border border-solid border-emerald-100 transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
+                    <button className="px-6 py-2 font-medium border border-solid border-emerald-100 transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
                         View Work
                     </button>
                 </div>
