8ae0742a8a4fb59a5eaf6264f037b76a136369da
tstyer <travisstyer.ts@gmail.com>
2026-09-18 13:31:40 +0100

    Add styling to the H1 and div for contact page

--- frontend/src/pages/contact_page/ContactPage.tsx
+++ frontend/src/pages/contact_page/ContactPage.tsx
@@ -5,9 +5,21 @@ import { Footer } from "../../components/footer/Footer"
 export function ContactPage() {
     return(
         <div className="max-w-7xl mx-auto px-4 sm:px-8 py-6">
-            
+
             <NavBar />
-            <h1>Contact Page</h1>
+
+        <div className="flex flex-col justify-between gap-8 pt-8 md:pt-16">
+            <div className="px-3 py-2">
+                <h1 className="font-heading font-semibold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">Shoot me a message:</h1>
+            </div>
+
+            <div>
+
+                <h2>CONTACT FORM</h2>
+                {/* contact form */}
+            </div>
+        </div>
+           
 
             <Footer />
         </div>
