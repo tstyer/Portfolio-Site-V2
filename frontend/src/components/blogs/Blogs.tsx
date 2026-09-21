3d33766a006d7d6b1a6ec43938725d8592665a92
tstyer <travisstyer.ts@gmail.com>
2026-09-21 12:22:42 +0100

    Add default blog cards

--- frontend/src/components/blogs/Blogs.tsx
+++ frontend/src/components/blogs/Blogs.tsx
@@ -23,12 +23,15 @@ export function Blogs() {
         <section className="pt-16 md:pt-24">
             
             <h2 className="font-heading font-bold text-3xl sm:text-4xl md:text-5xl mt-2">Blogs</h2>
+            <h2 className="font-text font-bold mt-2">Each blog will take you to its Substack page when clicked.</h2>
             <div className="border-t border-black/10 mt-6 mb-10 md:mb-12" />
             
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 {blogs.map((blog) => (
                     <div key={(blog.title)} className="border border-solid border-emerald-100 rounded-lg overflow-hidden bg-[#D6D3CC]/40">
-
+                        <div className="flex items-center justify-center h-48 bg-[#D6D3CC]" style={{ backgroundImage: `url(${blog.image})`}}>
+                            
+                        </div>
                     </div>
                 ))}
 
